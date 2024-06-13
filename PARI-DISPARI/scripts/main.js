440a24a46a9b0d77bb90766b94d75782089539e2
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-06-13 03:46:48 +0200

    feat(PARI-DISPARI/js): functions neded for main page script completed

--- PARI-DISPARI/scripts/main.js
+++ PARI-DISPARI/scripts/main.js
@@ -1,1 +1,14 @@
+// Random number generator between max and min (included)
+function randomNumberGenerator(min , max) {
+    return Math.floor((Math.random()) * (max - min + 1)) + min;
+}
 
+// Sum of 2 numbers
+function mySum(number1 , number2) {
+    return number1 + number2;
+}
+
+// Number comparison: if num1 is higher returns true, if num2 is higher or equal returns false
+function evenNumberCheck(number) {
+    return number % 2 === 0 ? true : false ;
+}
