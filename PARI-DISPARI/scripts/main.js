737b07462a4b6fd57e1b38b42d8e18164fb15558
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-06-13 03:47:32 +0200

    feat(PARI-DISPARI/js): page script completed

--- PARI-DISPARI/scripts/main.js
+++ PARI-DISPARI/scripts/main.js
@@ -1,3 +1,27 @@
+const userForm = document.getElementById('game-form');
+const textArea = document.getElementById('text-area');
+
+let playedCount = 0;
+
+userForm.addEventListener("submit" , 
+    function ()  {
+        const randomNumber = randomNumberGenerator (1 , 5);
+        const sumResult = mySum(parseInt(document.getElementById('user-number').value) , randomNumber);
+
+        const isGuessEven = document.getElementById('user-guess').value === 'even' ? true : false ;
+        const isResultEven = evenNumberCheck(sumResult);
+
+        textArea.innerHTML = `Il numero che è stato generato è ${randomNumber}, la somma dei due numeri è ${sumResult} e quindi `
+        textArea.innerHTML += isResultEven ? '<b>pari</b>.' : '<b>dispari</b>.';
+        textArea.innerHTML += '<br>'
+        textArea.innerHTML += isResultEven === isGuessEven ? 'Complimenti, <strong>hai vinto!</strong>' : 'Mi dispiace, <strong>hai perso!</strong>';
+        textArea.innerHTML += '<br>'
+        textArea.innerHTML += playedCount >= 10 ? 'Il gioco ti sta proprio piacendo allora, gioca pure quanto vuoi!' : ' Puoi comunque giocare ancora!';
+
+        playedCount++;
+    }
+);
+
 // Random number generator between max and min (included)
 function randomNumberGenerator(min , max) {
     return Math.floor((Math.random()) * (max - min + 1)) + min;
