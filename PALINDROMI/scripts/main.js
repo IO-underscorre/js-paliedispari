54bfb2452f9b61dd7ce517b8c07103b075662539
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-06-13 05:12:47 +0200

    feat(PALINDROMI/js): page script completed

--- PALINDROMI/scripts/main.js
+++ PALINDROMI/scripts/main.js
@@ -1,1 +1,24 @@
+const userForm = document.getElementById('text-form');
+const outputArea = document.getElementById('output-area');
 
+userForm.addEventListener("submit" , 
+    function ()  {
+        const userWord = document.getElementById('text-input').value;
+
+        outputArea.innerHTML = `La parola <strong>${userWord}</strong> `
+        outputArea.innerHTML += palindromeCheck(userWord) ? '<strong>è palindroma</strong>!' : '<strong>non è palindroma</strong>!';
+    }
+);
+
+// Text string checker for palindrome
+function palindromeCheck(word) {
+    let isPalindrome = true;
+
+    for(let i = 0 ; i < (word.length / 2) && isPalindrome ; i++) {
+        if (word[i] === word[word.length - i]) {
+            isPalindrome = false;
+        }
+    }
+
+    return isPalindrome;
+}
